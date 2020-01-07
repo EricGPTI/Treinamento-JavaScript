780c97149e866d5d344e34c8a79cfa0c3f3dbd54
Eric <eric.silva@nsprevention.com.br>
2020-01-07 20:26:14 -0300

    Exercício aula 12 resolvido

---
+++ JavaScript/aula12/index12.js
@@ -0,0 +1,16 @@
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+varI = varA;
+
+varA = varB;
+varB = varC;
+varC = varI;
+
+
+console.log(varA, varB, varC)
+
+let t = 'meu texto';
+console.log(t[0])
+console.log(`${t} um lindo dia`)
